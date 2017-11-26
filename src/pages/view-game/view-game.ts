8f17a4fd1f39adf1d9a5f891ad352aa9286dbd01
cwan <manwan_444@hotmail.com>
2017-11-26 20:07:35 +0700

    add view game func

--- src/pages/view-game/view-game.ts
+++ src/pages/view-game/view-game.ts
@@ -1,5 +1,13 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, AlertController, LoadingController } from 'ionic-angular';
+import { Loading } from 'ionic-angular/components/loading/loading';
+
+import { Validators, FormGroup, FormArray, FormBuilder } from '@angular/forms';
+import { Game } from './../../interface/Game';
+
+import { EditGamePage } from './../edit-game/edit-game';
+
+import { RestApiProvider } from './../../providers/rest-api/rest-api';
 
 /**
  * Generated class for the ViewGamePage page.
@@ -14,11 +22,191 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class ViewGamePage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  public game: Game;
+  public deleteGameQuestion = [];
+
+  private loader: Loading;
+  
+  public gameForm: FormGroup;
+
+  public minSelectabledate;
+  public maxSelectabledate;
+
+  public listFaculties;
+  public listMajors;
+
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams,
+    private restApiProvider: RestApiProvider,
+    public formBuilder: FormBuilder,
+    private alertCtrl: AlertController,
+    public loadingCtrl: LoadingController
+  ) {
+    this.game = navParams.get("game");
+    console.log("Game",this.game);
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad ViewGamePage');
+  ngOnInit(){
+    this.getListOfFaculties();
+
+    let d = new Date();
+    this.minSelectabledate = d.getFullYear();
+    this.maxSelectabledate = d.getFullYear()+1;
+
+    this.initGame();
+    this.gameForm.disable();
+  }
+
+  initGame(){
+    //Change NULL to empty 
+    if(this.game.Image == null){
+      this.game.Image = "";
+    }
+    if(this.game.Location_Latitude == null){
+      this.game.Location_Latitude = "";
+    }
+    if(this.game.Location_Longitude == null){
+      this.game.Location_Longitude = "";
+    }
+    if(this.game.FID == null){
+      this.game.FID = "-1";
+    }
+    if(this.game.MID == null){
+      this.game.MID = "-1";
+    }
+    //--
+    this.gameForm = this.formBuilder.group({
+      GID: this.game.GID.toString(),
+      Name: [this.game.Name.toString(), [Validators.required]],
+      Info: [this.game.Info.toString(), [Validators.required]],
+      Image: this.game.Image.toString(),
+      Time_Start: [this.convertTime(this.game.Time_Start), [Validators.required]],
+      Time_End: [this.convertTime(this.game.Time_End), [Validators.required]],
+      State: this.game.State.toString(),
+      Location_Latitude: this.game.Location_Latitude.toString(),
+      Location_Longitude: this.game.Location_Longitude.toString(),
+      Game_Question: this.formBuilder.array([]),
+      MID: [this.game.MID.toString(), [Validators.required]],
+      FID: [this.game.FID.toString(), [Validators.required]]
+    });
+    //set major 
+    this.hintMajors(Number(this.game.FID));
+    this.gameForm.patchValue({MID: this.game.MID.toString()});
+    //set game question
+    this.initGameQuestion();
+  }
+
+  convertTime(time: string){
+    let temp = time.split(" ");
+    return temp[0]+"T"+temp[1]+".000Z"
+  }
+
+  initGameQuestion(){
+    this.restApiProvider.getGameQuestion(Number(this.game.GID))
+    .then(result => {
+      let json: any = result;
+      const gameControl = <FormArray>this.gameForm.controls["Game_Question"];
+      json.forEach(q => {
+        let gq = this.formBuilder.group({
+          QID: q.QID.toString(),
+          Question: [q.Question.toString(), [Validators.required]],
+          Answer_Choice: this.formBuilder.array([]),
+          Right_Choice: [q.Right_Choice.toString(), [Validators.required]]
+        });
+        
+        this.initAnswerChoice(q.QID, gq);
+        
+        gameControl.push(gq);
+      });
+    })
+    .catch(error =>{
+      console.log("ERROR API : getGameQuestion",error);
+    })
+  }
+
+  initAnswerChoice(qid: number, gq: FormGroup){
+    this.restApiProvider.getAnswerChoice(0,qid)
+    .then(result => {
+      let json: any = result;
+      const choiceControl = <FormArray>gq.controls["Answer_Choice"];
+      json.forEach(c => {
+        choiceControl.push(this.formBuilder.group({
+          CID: c.CID.toString(),
+          Choice: [c.Choice.toString(), [Validators.required]],
+        }));
+      });
+    })
+    .catch(error =>{
+      console.log("ERROR API : getAnswerChoice",error);
+    })
+  }
+
+  initNewGameQuestion(){
+    return this.formBuilder.group({
+      Question: ["", [Validators.required]],
+      Answer_Choice: this.formBuilder.array([
+        this.initNewAnswerChoice(),this.initNewAnswerChoice(),this.initNewAnswerChoice(),this.initNewAnswerChoice(),
+      ]),
+      Right_Choice: ["", [Validators.required]]
+    });
+  }
+
+  initNewAnswerChoice(){
+    return this.formBuilder.group({
+      Choice: ["", [Validators.required]],
+    });
+  }
+
+  getListOfFaculties(){
+    this.restApiProvider.getFaculties()
+    .then(result => {
+      this.listFaculties = result;
+    })
+    .catch(error =>{
+      console.log("ERROR API : getFaculties",error);
+    })
+  }
+
+  hintMajors(fid: number){
+    this.gameForm.patchValue({MID:"-1"});
+    if(fid == -1){
+      this.listMajors = null;
+      return;
+    }
+    this.restApiProvider.getMajorsInFaculty(fid)
+    .then(result => {
+      this.listMajors = result;
+    })
+    .catch(error =>{
+      console.log("ERROR API : getMajorsInFaculty",error);
+    })
+  }
+
+  gameEdit(){
+    console.log("editGame");
+    let game = this.game;
+    
+    this.navCtrl.push(EditGamePage, {game: game, "parentPage":  this.navParams.get("parentPage")});
+  }
+
+  presentAlert(message) {
+    let alert = this.alertCtrl.create({
+      title: 'Alert!',
+      subTitle: message,
+      enableBackdropDismiss: false,
+      buttons: [{
+        text: 'Ok'
+      }]
+    });
+    alert.present();
+  }
+
+  presentLoading() {
+    this.loader = this.loadingCtrl.create({
+      content: "Please wait..."
+    });
+    this.loader.present();
   }
 
 }
